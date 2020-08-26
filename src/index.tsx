67cae4cc1a808d9622f2002023cab5e4a10592cc
Usman Ali <usmanusman136@hotmail.com>
2020-08-26 17:00:50 +0100

    minor tweaks and removed unused packages

--- src/index.tsx
+++ src/index.tsx
@@ -4,39 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-
-const cache = new InMemoryCache();
-
-export const client = new ApolloClient({
-  cache,
-  uri: 'http://localhost:4000/', //URL of the GraphQL server
-  request: (operation) => {
-    const token = localStorage.getItem('token');
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : '',
-      },
-    });
-  },
-});
-
-const initialState = {
-  authenticated: false,
-  id: null,
-  email: null,
-  userType: null,
-};
-cache.writeData({ data: initialState });
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ApolloProvider client={client}>
         <App />
-      </ApolloProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
